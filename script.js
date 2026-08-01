a065a7fb27edb181aeae98531be07446aea85b77
yogurtshaked <kristiannegabas@gmail.com>
2026-08-01 23:57:53 +0800

    Updated mobile responsiveness

--- script.js
+++ script.js
@@ -94,3 +94,12 @@ window.addEventListener("load", () => {
 });
 
 
+window.addEventListener("load", () => {
+  const loader = document.getElementById("loader");
+
+  loader.classList.add("hidden");
+
+  setTimeout(() => {
+    loader.remove();
+  }, 600);
+});
