bc6023fbfac80e43b540a5236ddbcd1fe91aa9d8
yogurtshaked <kristiannegabas@gmail.com>
2026-08-14 14:53:34 +0800

    Updated Certifications Card Design

--- script.js
+++ script.js
@@ -135,38 +135,6 @@ window.addEventListener("load", () => {
     loader.remove();
   }, 600);
 });
-const carousel = document.querySelector(".certifications-container");
-
-if (carousel) {
-  let isDown = false;
-  let startX;
-  let scrollLeft;
-
-  carousel.addEventListener("mousedown", (e) => {
-    isDown = true;
-    carousel.classList.add("dragging");
-    startX = e.pageX - carousel.offsetLeft;
-    scrollLeft = carousel.scrollLeft;
-  });
-
-  carousel.addEventListener("mouseleave", () => {
-    isDown = false;
-    carousel.classList.remove("dragging");
-  });
-
-  carousel.addEventListener("mouseup", () => {
-    isDown = false;
-    carousel.classList.remove("dragging");
-  });
-
-  carousel.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - carousel.offsetLeft;
-    const walk = (x - startX) * 1.5; // drag speed multiplier
-    carousel.scrollLeft = scrollLeft - walk;
-  });
-}
 
 
 // ==========================================
